535189c780fb00d4a7821dc7ed46b2f04fdba3e4
Daniel Handojo <dhand002@sledge.cs.ucr.edu>
2017-11-03 11:10:17 -0700

    mytest

--- myTest.c
+++ myTest.c
@@ -1,8 +1,9 @@
+#include "types.h"
 #include "syscall.h"
 #include "user.h"
 
 int main(int argc, char *argv[]) {
-    int i = info(1);
+    int i = 5;//info(1);
     printf(1, "%i\n", i);
     return 0;
 }
--- user.h
+++ user.h
@@ -23,6 +23,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int info(int);  // DANIEL
 
 // ulib.c
 int stat(char*, struct stat*);
